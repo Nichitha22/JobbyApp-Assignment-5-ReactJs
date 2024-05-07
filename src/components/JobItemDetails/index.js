4940864cba7cc906ca8dd0c35ba2b7b66cc6f95e
Nichitha22 <munjalanichitha2002@gmail.com>
2024-05-07 10:31:13 +0000

    Enhancement of Jobby App

--- src/components/JobItemDetails/index.js
+++ src/components/JobItemDetails/index.js
@@ -5,7 +5,6 @@ import {IoLocationSharp} from 'react-icons/io5'
 import {BsFillBriefcaseFill} from 'react-icons/bs'
 import {FiExternalLink} from 'react-icons/fi'
 import Loader from 'react-loader-spinner'
-
 import Header from '../Header'
 import SimilarJobCard from '../SimilarJobCard'
 
